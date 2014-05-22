52527e78791953e49d754fb2ae53da798a626ad1
lc-petranussdorfer <petra.nussdorfer@learnchamp.com>
2014-05-22 12:48:48 +0200

    Displayed the user answer in the linked component

--- js/adapt-contrib-linkedOpenTextInput.js
+++ js/adapt-contrib-linkedOpenTextInput.js
@@ -13,12 +13,12 @@
 define(function(require) {
     var QuestionView = require('coreViews/questionView');
     var Adapt = require('coreJS/adapt');
-     var OpenTextInput = require('components/adapt-contrib-openTextInput/js/adapt-contrib-openTextInput');
+    var OpenTextInput = require('components/adapt-contrib-openTextInput/js/adapt-contrib-openTextInput');
 
 
     var LinkedOpenTextInput = QuestionView.extend({
         events: {
-           
+
             "click .linkedopentextinput-widget .button.model": "onModelAnswerClicked",
             "click .linkedopentextinput-widget .button.user": "onUserAnswerClicked",
         },
@@ -29,44 +29,52 @@ define(function(require) {
             this.setCompletionStatus();
             this.listenToLinkedModel();
 
+            // Check if the original component is already submitted
+            if (this.model.get('_linkedModel').get('_isSubmitted')) {
+                // Show linked component, display user answer
+                this.$(".linkedopentextinput-useranswer").text(this.model.get('_linkedModel').get('_userAnswer'));
+            }
 
- if(this.model.get('_linkedModel').get('_isSubmitted')) {
-                console.log("Submitted in postRender is true");
-            } 
 
-            // Read the last saved answer and paste it into the textarea
-            this.$(".linkedopentextinput-item-textbox").val(this.getUserAnswer());
-
-            this.$(".linkedopentextinput-useranswer").text(this.model.get('_linkedModel').get('_userAnswer'));
 
 
             QuestionView.prototype.postRender.apply(this);
         },
         preRender: function() {
             this.setupLinkedModel();
-           this.model.set('_isEnabled', this.model.get('_linkedModel').get('_isSubmitted'));
+            this.model.set('_isEnabled', this.model.get('_linkedModel').get('_isSubmitted'));
+            console.log("Prerender: " + this.model.get('_linkedModel').get('_userAnswer'));
 
-    
+
         },
         setupLinkedModel: function() {
-            var linkedModel = Adapt.components.findWhere({_id: this.model.get('_linkedToId')});
+            var linkedModel = Adapt.components.findWhere({
+                _id: this.model.get('_linkedToId')
+            });
             this.model.set('_linkedModel', linkedModel);
+            console.log("setupLinkedModel: " + this.model.get('_linkedModel'));
         },
-       listenToLinkedModel: function() {
-        console.log("Bin in listenToLinkedModel");
+        listenToLinkedModel: function() {
             this.listenTo(this.model.get('_linkedModel'), 'change:_isSubmitted', this.onLinkedSubmittedChanged);
-
-          console.log("Is submitted in listenToLinkeModel: " + this.model.get('_linkedModel').get('_isSubmitted'));
+            this.listenTo(this.model.get('_linkedModel'), 'change:_userAnswer', this.onLinkedUserAnswerChanged);
 
         },
-         onLinkedSubmittedChanged: function(linkedModel) {
-            if(linkedModel.get('_isSubmitted')) {
-                        console.log("Is submitted in onLinkedSubmittedChanged: " + this.model.get('_linkedModel').get('_isSubmitted'));
+        onLinkedSubmittedChanged: function(linkedModel) {
+            if (linkedModel.get('_isSubmitted')) {
+
+                /* this.$(".linkedopentextinput-useranswer").text(this.model.get('_linkedModel').get('_userAnswer'));
+                 */
+
+                console.log("Is submitted in onLinkedSubmittedChanged: " + this.model.get('_linkedModel').get('_userAnswer'));
 
             }
 
         },
-       
+
+        onLinkedUserAnswerChanged: function(linkedModel) {
+            this.$(".linkedopentextinput-useranswer").text(this.model.get('_linkedModel').get('_userAnswer'));
+        },
+
         onModelAnswerShown: function() {
             this.$(".linkedopentextinput-item-textbox").val(this.model.get('modelAnswer'));
         },
@@ -74,7 +82,7 @@ define(function(require) {
             this.$(".linkedopentextinput-item-textbox").val(this.getUserAnswer());
         },
         getUserAnswer: function() {
-           /* var identifier = this.model.get('_id') + "-LinkedOpenTextInput-UserAnswer";
+            /* var identifier = this.model.get('_id') + "-LinkedOpenTextInput-UserAnswer";
             var userAnswer = '';
             if (this.supports_html5_storage()) {
                 userAnswer = localStorage.getItem(identifier);
@@ -84,7 +92,7 @@ define(function(require) {
             return userAnswer;*/
         },
         onComplete: function(parameters) {
-           /* this.model.set({
+            /* this.model.set({
                 _isComplete: true,
                 _isEnabled: false,
             });
@@ -93,7 +101,7 @@ define(function(require) {
             this.showUserAnswer();
             Adapt.trigger('questionView:complete', this);*/
         },
-        
+
         markQuestion: function() {}
     });
 
